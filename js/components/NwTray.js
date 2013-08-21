943fc6b04475db63ff6a38e79dba591d3a64bde4
Tony Osibov <tony.osibov@gmail.com>
2013-08-21 10:19:13 -0700

    udpated tray code still needs work

--- js/components/NwTray.js
+++ js/components/NwTray.js
@@ -8,7 +8,59 @@ OJ.importCss('nw.components.NwTray');
 OJ.extendComponent(
   'NwTray', [OjComponent],
   {
-    '_template' : 'nw.components.NwTray'
+    '_template' : 'nw.components.NwTray',
+
+
+		'_onTrayUp' : function(evt){
+			this.hideTray();
+		},
+
+
+    'getPosition' : function(){
+      return this.container.getX();
+    },
+
+    'hideTray' : function(){
+        var amount = this.getWidth() * -.6;
+
+        if(this.tray.getX() == amount){
+          return;
+        }
+
+        this._startTrayAnim(amount, 0);
+
+        this.container.removeEventListener(OjMouseEvent.CLICK, this, '_onTrayUp');
+    },
+
+    'revealTray' : function(val){
+        var w = this.getWidth() * .6;
+
+        this.tray.setX(Math.max(Math.min(val - w, 0), -(w)));
+        this.container.setX(Math.min(Math.max(val, 0), w));
+
+        this.container.addEventListener(OjMouseEvent.CLICK, this, '_onTrayUp');
+    },
+
+    'showTray' : function(){
+        if(!this.tray.getX()){
+            return;
+        }
+
+        this._startTrayAnim(0, this.getWidth() * .6);
+    },
+
+    'toggle' : function(/*val*/){
+        var args = arguments,
+            w = this.getWidth(),
+            val = args.length? args[0] : (this.container.getX() ? 0 : w);
+
+        if(val > w * .3){
+            this.showTray();
+        }
+        else{
+            this.hideTray();
+        }
+    }
   },
   {
     '_TAGS' : ['tray']
--- nw.dev.js
+++ nw.dev.js
@@ -1905,7 +1905,46 @@ OJ.extendComponent(
 OJ.extendComponent(
   'NwTray', [OjComponent],
   {
-    '_template' : '<div><div var=tray></div><div var=container></div></div>'
+    '_template' : '<div><div var=tray></div><div var=container></div></div>',
+
+		'_onTrayUp' : function(evt){
+			this.hideTray();
+		},
+
+    'getPosition' : function(){
+      return this.container.getX();
+    },
+    'hideTray' : function(){
+        var amount = this.getWidth() * -.6;
+        if(this.tray.getX() == amount){
+          return;
+        }
+        this._startTrayAnim(amount, 0);
+        this.container.removeEventListener(OjMouseEvent.CLICK, this, '_onTrayUp');
+    },
+    'revealTray' : function(val){
+        var w = this.getWidth() * .6;
+        this.tray.setX(Math.max(Math.min(val - w, 0), -(w)));
+        this.container.setX(Math.min(Math.max(val, 0), w));
+        this.container.addEventListener(OjMouseEvent.CLICK, this, '_onTrayUp');
+    },
+    'showTray' : function(){
+        if(!this.tray.getX()){
+            return;
+        }
+        this._startTrayAnim(0, this.getWidth() * .6);
+    },
+    'toggle' : function(/*val*/){
+        var args = arguments,
+            w = this.getWidth(),
+            val = args.length? args[0] : (this.container.getX() ? 0 : w);
+        if(val > w * .3){
+            this.showTray();
+        }
+        else{
+            this.hideTray();
+        }
+    }
   },
   {
     '_TAGS' : ['tray']
